440a0401935e6a53fe37f5d603177881598b4157
771400163 <771400163@SALA602PC19.fspoaeduc.com.br>
2016-06-01 11:57:45 -0300

    ajustes no index e style

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
+<?php
+session_start();
+?>
 
-<html xmlns="http://www.w3.org/1999/xhtml" lang="pt-br" xml:lang="pt-br">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!DOCTYPE html>
+<html  xmlns="http://www.w3.org/1999/xhtml" lang="pt-br" xml:lang="pt-br">
     <head>
-        <title>Locadora de Veículos</title>
-        <link href="config/style.css" rel="stylesheet" type="text/css" />
+        <meta charset="UTF-8" http-equiv="Content-Type">
+            <style type="text/css">
+                @import 'config/style.css';
+            </style>
+            <script type="text/javascript">
+
+                window.onload = function () {
+                    document.getElementById("categoria").onchange = function () {
+                        this.parentNode.action = this.value + ".php";
+                    }
+                }
+            </script>
+            <title>Locadora de Veículos</title>
+            <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;"/>
     </head>
-    <body><!-- define todo conteúdo da pág. -->
-        <div id="cabecalho">
+
+    <body>
+        <div id="corpo">
             <div id="topo">
                 <h1>
                     .: Locadora de Veículos :.<p>
@@ -16,13 +32,38 @@
                         </script>
                 </h1>
             </div>
-        </div>
-        <div id="conteudo" align="center">
-            <div id="um">
-                <p>
-                    <a href="listaVeicDisp.html">-- Visualiza Veículos --</a>
-                </p>
-            </div>
-        </div>
-    </body>
-</html>
+
+            <form action="" name="formPesquisa" id="formPesquisa" method="post">
+
+                <input type="text" class="campo" name="nome" id="nome" placeholder="Procure por um tipo de etiqueta" required>
+
+                    <select class="campo" id="categoria" name="categoria" required>
+                        <option value="" disabled selected="" >Escolha uma categoria de busca</option>
+                        <option value="action/buscado_altura">Altura</option>
+                        <option value="action/buscado_largura">Largura</option>
+                        <option value="action/buscado_fabricante">Fabricante/Marca</option>
+                        <option value="action/buscado_nome">Modelo/Nome</option>
+                        <option value="action/buscado_tipopapel">Tipo do Papel</option>
+                    </select>
+                    <br><br>
+                            <input name="acoes" id="acoes"  type="submit" class="botao_pes" value="Pesquisar" title="Irá pesquisar todos os tipos de etiquetas que você selecionou acima."><br><br><br>
+
+                                            <?php
+                                            if (!isset($_SESSION['id'])) {
+                                                ?>
+                                                | <a href="visao/login.php" class="jujuba">Efetuar login</a> |
+                                                <a href="visao/verTodasEtiquetas.php" class="jujuba">Visualizar etiquetas</a> |
+                                                <?php
+                                            } else {
+                                                ?>
+                                                | <a href="sair.php" class="jujuba">Sair</a> |
+                                                <a href="visao/cadUsuario.php" class="jujuba">Cadastrar usuario</a> |
+                                                <a href="visao/cadFabricanteEtiqueta.php" class="jujuba">Cadastrar Fabricante/Etiqueta</a> |
+                                                <a href="visao/verTodasEtiquetas.php" class="jujuba">Visualizar etiquetas</a> |
+                                                <?php
+                                            }
+                                            ?>	
+                                            </form>
+                                            </div>
+                                            </body>
+                                            </html>
