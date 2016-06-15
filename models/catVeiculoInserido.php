a06afd5476d59c62d0a5d8d89612b5256a95dcc1
771400163 <771400163@SALA602PC19.fspoaeduc.com.br>
2016-06-15 11:08:22 -0300

    INSERE CATEGORIA PRONTO

---
+++ models/catVeiculoInserido.php
@@ -0,0 +1,26 @@
+<html  xmlns="http://www.w3.org/1999/xhtml" lang="pt-br" xml:lang="pt-br">
+    <head>
+        <meta charset="UTF-8" http-equiv="Content-Type">
+            <style type="text/css">
+                @import '../config/style.css';
+            </style>
+            <title>Veículo cadastrado</title>
+            <meta http-equiv="refresh" content="2 ;../views/listaVeicDisp.php"/>
+    </head>
+    <body>
+        <?php
+        $descCatVeiculo = $_POST['descCatVeiculo'];
+        if ($descCatVeiculo == '')
+            print('<script type="text/javascript">alert("Faltou preencher Categoria de Veículo")</script>');
+        else {
+            require("../controller/conecta.inc");
+            conecta_bd() or die("Não é possível conectar-se ao servidor.");
+            mysql_query("insert into categoriaveiculo (descCatVeiculo) values ('$descCatVeiculo')")
+                    or die('<script type="text/javascript">alert("Não é possível inserir o categoria veículo!")</script>');
+            print('<script type="text/javascript">alert("Categoria Veículo inserida com sucesso: ")</script>');
+        }
+        ?>
+        <p><a href="inserirCatVeiculo.php">Voltar</a></p>
+        <p><a href="../views/listaVeicDisp.php">Mostrar</a></p>
+    </body>
+</html>
